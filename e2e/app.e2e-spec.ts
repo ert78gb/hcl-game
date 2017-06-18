e1a9efa45dbc8b18756dfd90054c20dcf6f70c86
Róbert Kiss <ert78gb@gmail.com>
2017-06-18 07:56:30 +0200

    refactor: Create page-objects folder and move po-s into it

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,9 @@
-import { HclGamePage } from './app.po';
+import { HclGamePage } from './page-objects/app.po';
 
 describe('hcl-game App', () => {
   let page: HclGamePage;
 
   beforeEach(() => {
     page = new HclGamePage();
-  });
-
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!!');
   });
 });
